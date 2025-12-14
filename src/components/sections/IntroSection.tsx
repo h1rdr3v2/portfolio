c22880579a995c3b4c116bbad802dbc9ef550fdf
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-12-14 07:40:29 +0100

    minor update

--- src/components/sections/IntroSection.tsx
+++ src/components/sections/IntroSection.tsx
@@ -4,18 +4,20 @@ import Image from "next/image";
 const IntroSection = () => {
     return (
         <section className="w-full">
-            <div className="w-full overflow-hidden relative rounded-xl mb-12" style={{ height: "200px", width: '100%'}} >
-                <Image
-                    src={me}
-                    alt="My profile picture"
-                    fill={true}
-                    style={{objectFit: "cover"}}
-                    sizes="(max-width: 100%) 400px"
-                    priority
-                />
-            </div>
-            <h1 className="text-lg font-medium">Destiny Ezenwata</h1>
-            <span className="text-gray-500 text-sm -mt-0.5 font-medium">Software Developer {"<>"} Mobile App Developer</span>
+			<div className="flex flex-row gap-4 items-center">
+				<div className="overflow-hidden rounded-full w-[70px] h-[70px]" >
+					<Image
+						src={me}
+						alt="My profile picture"
+						objectFit="cover"
+						priority
+					/>
+				</div>
+				<div>
+					<h1 className="text-lg font-medium">Destiny Ezenwata</h1>
+					<span className="text-gray-500 text-sm -mt-0.5 font-medium">Software Developer {"<>"} Mobile App Developer</span>
+				</div>
+			</div>
         </section>
     );
 };
