f5875e0e05185d654d5b4f805b4738b815e7087a
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2026-05-11 10:56:35 +0100

    fixed prod settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite"
+import { nitro } from "nitro/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import tsConfigPaths from "vite-tsconfig-paths"
@@ -15,6 +16,8 @@ export default defineConfig({
 				routesDirectory: "app",
 			},
 		}),
+		nitro(),
 		viteReact(),
 	],
+	nitro: {},
 })
